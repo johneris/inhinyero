0e149ceadf6dd0533b2f964cb0d52ed31ceb62d4
John Eris Villanueva <johnerisvillanueva@gmail.com>
2019-11-13 16:34:32 +0800

    Nuke: show list of files and ask before deleting

--- lib/inhinyero/commands/nuke.rb
+++ lib/inhinyero/commands/nuke.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../command'
+require "tty-prompt"
+require 'fileutils'
 
 module Inhinyero
   module Commands
@@ -11,7 +13,33 @@ module Inhinyero
       end
 
       def execute(input: $stdin, output: $stdout)
-        Dir.each_child("#{@dir}") {|x| puts "#{x}" }
+        # Get *.xcodeproj and *.xcworkspace files
+        files = Dir[File.join(
+            @dir, '**', '*'
+        )].filter { |p|
+          %w(.xcodeproj .xcworkspace).include? File.extname(p) and not(["project.xcworkspace"].include? File.basename(p))
+        }
+
+        if files.empty?
+          puts "There are no *.xcodeproj and *.xcworkspace files found."
+          return
+        end
+
+        file_names = files.map { |file| File.basename(file) }
+        map_file_name_file = files.each_with_object({}) do |file, map|
+          map[File.basename(file)] = file
+        end
+
+        # Ask what to delete
+        prompt = TTY::Prompt.new
+        selected_file_names = prompt.multi_select("XCode Projects and Workspace", file_names)
+
+        # Delete selected files
+        selected_file_names.each { |file_name|
+          puts "Deleting #{file_name}"
+          FileUtils.rm_rf(map_file_name_file[file_name])
+        }
+
         output.puts "OK"
       end
     end
